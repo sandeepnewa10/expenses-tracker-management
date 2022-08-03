96e8f23293cb66a01ed88f14bd7cd3a52870d33e
sandeepnewa10 <sandeepnewa@gmail.com>
2022-08-03 16:06:41 +1000

    port updated

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ import "dotenv/config"
 import express from "express";
 const app = express();
 import cors from "cors";
-const PORT = 8000;
+const PORT = process.env.PORT  || 8000;
 
 import path from 'path'
 
